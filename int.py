adb09ba9b94196d43769467a7cc32ae1576bed58
ITUMOR <huma20050002hti28@gmail.com>
2021-03-25 17:28:09 +0200

    Update int.py

--- int.py
+++ int.py
@@ -6,3 +6,8 @@ import os
 os.system('kubeadm init --apiserver-advertise-address $(hostname -i) --pod-network-cidr 10.5.0.0/16')
 os.system('kubectl apply -f https://raw.githubusercontent.com/cloudnativelabs/kube-router/master/daemonset/kubeadm-kuberouter.yaml')
 os.system('kubectl get no')
+
+import subprocess
+
+list_dir = subprocess.Popen(["ls", "-l"])
+list_dir.wait()
